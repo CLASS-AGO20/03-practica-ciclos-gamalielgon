6374f04e9acb073c10b9d4e2f9d5e6af988f0a2d
gamalielgon <ggonzalez26@ucol.mx>
2020-12-09 16:21:55 -0600

    Added obtenerDivisibles

--- src/main.js
+++ src/main.js
@@ -16,8 +16,31 @@ export default class App {
         }
         return aletras;
     }
+    obtenerDivisibles(numero){
+        let divisores = 0;
+        let div = numero;
+        let i = 0;
+        do{
+            if(numero%div==0){
+                divisores++;
+            }
+            i++;
+            div = div - 1;
+
+        } while(i <= numero);
+        return divisores;
+    }
+    elevar(numero, potencial){
+        let potencia = 1;
+        for(let i = 1; i <= potencial; i++){
+            potencia = numero * potencia;
+        }
+        return potencia;
+    }
 }
 
 let app = new App();
 console.log(app.factorial(4));
 console.log(app.convertirAString(4));
+console.log(app.obtenerDivisibles(4));
+console.log(app.elevar(4,4));
