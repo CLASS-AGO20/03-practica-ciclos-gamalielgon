a1721203bc6c742773f1729173b60f93ee13f8b3
gamalielgon <ggonzalez26@ucol.mx>
2020-12-09 15:46:22 -0600

    Added factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-export default class App {}
+export default class App {
+
+    factorial(numero) {
+        let suma = 1;
+        for(let i = 1; i <= numero; i++){
+            suma = i * suma;
+        }
+        return suma;
+    }
+}
 
 let app = new App();
+console.log(app.factorial(4));
