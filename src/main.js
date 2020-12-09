7eacb2498a320e04d86c3975c7c7d1fed7f2c0ad
gamalielgon <ggonzalez26@ucol.mx>
2020-12-09 15:50:33 -0600

    Added convertirAString

--- src/main.js
+++ src/main.js
@@ -7,7 +7,17 @@ export default class App {
         }
         return suma;
     }
+    convertirAString(numero){
+        let aletras = "";
+        let i = 1;
+        while(i <= numero){
+            aletras = "*" + aletras;
+            i++;
+        }
+        return aletras;
+    }
 }
 
 let app = new App();
 console.log(app.factorial(4));
+console.log(app.convertirAString(4));
